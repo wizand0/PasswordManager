442f68c2b6aed4a01e1609b81e79fb0cc1b747b3
wizand0 <xabor@yandex.ru>
2024-08-29 15:38:06 +0300

    README.MD

---
+++ app/src/main/java/ru/wizand/passwordmanager/encryptingKey.java
@@ -0,0 +1,46 @@
+package ru.wizand.passwordmanager;
+
+import static ru.wizand.passwordmanager.AesCbcWithIntegrity.generateKeyFromPassword;
+import static ru.wizand.passwordmanager.AesCbcWithIntegrity.generateSalt;
+import static ru.wizand.passwordmanager.AesCbcWithIntegrity.saltString;
+
+import android.util.Log;
+
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+
+class EncryptionLibrary {
+
+    public static String encryptingKey(String inputString, String salt){
+
+
+        //Generating salt
+//         String salt = null;
+//        String salt = "4ooMz5/R/xl8df9Iife5GWmBuYaqBa54ESgTnZUOpkgNWKQ82i8OEMqoK/UwfGx8+DaRgCjidmqHYcCeL2OG3SqWjDAqukJRWCAAiBZGUGH6FdB4VqzTrg2Gp9Tbu/rbgt4tUflbPv9qQZ4C4aYs0hZBIKIguhuHqXybl0+ZvzQ=";
+//        try {
+//            salt = saltString(generateSalt());
+//            Log.i("testing_input_salt", salt);
+//        } catch (GeneralSecurityException e) {
+//            throw new RuntimeException(e);
+//        }
+        //Generating Master key
+        String key = null;
+
+        try {
+
+            key = String.valueOf(generateKeyFromPassword(String.valueOf(inputString), salt));
+
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        }
+        return key;
+    }
+
+    public static String generatingSalt() throws GeneralSecurityException {
+        String salt = "";
+        salt = (String)saltString(generateSalt());
+        Log.i("testing_input_salt", salt);
+
+        return salt;
+    }
+}
